7a9028f862478cf3fcabd334fafb7d380855cec7
klinsmann505 <klinsmannvasquez2@gmail.com>
2026-06-01 12:40:52 -0600

    Add saludar_y_sumar function to funciones2.py

---
+++ funciones2.py
@@ -0,0 +1,7 @@
+def saludar_y_sumar(nombre, num1, num2=0):
+    print("Saludos", nombre)
+    print("Resultado de la suma", num1+num2)
+
+saludar_y_sumar("Rodrigo", 10, 5)
+saludar_y_sumar("Juan", 2)
+saludar_y_sumar("Pedro", 800, 5000)
