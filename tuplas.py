ab3e050accb6cf239edba161db77c726417f6f24
klinsmann505 <klinsmannvasquez2@gmail.com>
2026-06-01 12:43:18 -0600

    Create tuplas.py with tuple examples
    Added a new Python file explaining tuples and demonstrating their usage with examples.

---
+++ tuplas.py
@@ -0,0 +1,12 @@
+#Tuplas muy similares a las listas
+#No pueden modificarse
+#Se utilizan parentesis en lugar de []
+
+lista = ["Rodrigo", "Juan"] #Constantemente agregando informacion
+tupla = ("Rodrigo", "Juan")
+
+videojuegos = []
+#Nombre, Genero, Año
+videojuegos.append( ("Final Fantasy 7", "JRPG", 1997) )
+videojuegos.append( ("Outer Wilds", "Aventura", 2019) )
+print(videojuegos)
