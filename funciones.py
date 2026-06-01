88350c1669cbda701fba6789beedeec15bc46be8
klinsmann505 <klinsmannvasquez2@gmail.com>
2026-06-01 12:39:37 -0600

    Add greeting and temperature conversion functions
    Added functions for greeting and temperature conversion.

---
+++ funciones.py
@@ -0,0 +1,16 @@
+#Pre-definidas print(), type(), range(), input()
+
+def saludar(nombre):
+    print("Hola", nombre)
+
+#Celsius a Fahrenheit: (C * 1.8) + 32
+def convertir_a_fahrenheit(c):
+    return (c * 1.8) + 32
+
+#saludar("Rodrigo")
+#saludar("Juan")
+#saludar("Sergio")
+
+print( convertir_a_fahrenheit(10) )
+print( convertir_a_fahrenheit(30) )
+print( convertir_a_fahrenheit(80) )
