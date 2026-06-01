9a0ad48068e3ec50163b88fcb546bbf97a2a5587
klinsmann505 <klinsmannvasquez2@gmail.com>
2026-06-01 12:37:49 -0600

    Add example of dictionary usage in diccionarios.py
    Add a dictionary example with a person and check for a key.

---
+++ diccionarios.py
@@ -0,0 +1,15 @@
+#lista de elementos llave:valor
+#Arreglos asociativos
+
+persona = {"nombre": "Rodrigo",
+           "edad": 800,
+           "apellido": "Montemayor"}
+
+
+persona["apodo"] = "Ringa Tech"
+
+#print(persona.items())
+
+print ("direccion" in persona)
+
+#for key, value in persona.items():
