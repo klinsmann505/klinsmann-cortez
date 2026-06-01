a1ee2bb365f9f1952cefa9687b2f748d28820138
klinsmann505 <klinsmannvasquez2@gmail.com>
2026-06-01 12:34:35 -0600

    Add conditional greeting logic in condicionales.py

---
+++ condicionales.py
@@ -0,0 +1,12 @@
+#Logica Boolean Booleano Bool
+#Verdadero / Falso: True / False
+#dato = 5 < 5
+#> < ==
+
+print("Ingresa tu nombre")
+nombre = input()
+
+if nombre == "Rodrigo":
+    print("Bienvenido Rodrigo")
+else:
+    print("Que nombre tan extraño")
