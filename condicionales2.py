4f445e023956799ee4a435b41241ab2aeaec1d2b
klinsmann505 <klinsmannvasquez2@gmail.com>
2026-06-01 12:35:53 -0600

    Add conditional logic based on user name input

---
+++ condicionales2.py
@@ -0,0 +1,17 @@
+print("Escribe tu nombre:")
+nombre = input()
+
+#print("Escribe tu edad")
+#edad = int(input())
+
+#elif
+#Operadores Lógicos
+#and (y)  /  or (o)
+#and: Todas las expresiones sean True
+#or: Con que una de las expresiones sea True
+#> <   >=   <=
+
+if nombre == "Rodrigo" or nombre == "Santiago":
+    print("Me gusta tu nombre")
+else:
+    print("Que nombre tan extraño")
