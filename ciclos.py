b59d5b7df16954bf3d29c18f2e5210de2d19e815
klinsmann505 <klinsmannvasquez2@gmail.com>
2026-06-01 12:33:01 -0600

    Add ciclos.py with while and for loop examples

---
+++ ciclos.py
@@ -0,0 +1,35 @@
+# Ciclo, iteracion, bucle
+
+# while
+"""
+i = 0
+while i < 10:
+    if i < 5:
+        print("El número", i, "es menor a 5")
+    else:
+        print("El número", i, "es mayor o igual a 5")
+    i += 1
+
+print("Terminó la iteración")
+"""
+
+# for x in range(1, 11):
+    #print(x)
+
+while True:
+     print("Escribe la opción deseada")
+     print("1: Saludar")
+     print("2: Salir")
+    
+     respuesta = int(input())
+    
+if respuesta == 1:
+        print("Saludos terrícola!")
+elif respuesta == 2:
+       "break"
+
+       print("terminando programa")
+
+
+
+    
