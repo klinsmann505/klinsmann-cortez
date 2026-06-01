4b2c9a470fe69f73b18fac19bfd0e493ace73c50
klinsmann505 <klinsmannvasquez2@gmail.com>
2026-06-01 12:44:20 -0600

    Add variable.py with basic variable examples

---
+++ variable.py
@@ -0,0 +1,15 @@
+# soy un comentario
+#strings, cadenas, textos
+#numericos int (integer, entero) float (punto flotante)
+
+nombre = "Rodrigo"
+apellido = "Montemayor"
+edad = 20
+
+print(nombre, apellido, edad)
+
+#edad -= edad /3
+
+print(nombre, apellido, edad)
+
+# print(type(edad))
